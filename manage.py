3136802e0b9e9cdccbc8b199c7ec6e5834ad7229
Matt <mhk4g@virginia.edu>
2015-10-27 01:17:08 -0400

    Reconfiguring file structure for heroku deployment

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team16.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "team16project.settings")
 
     from django.core.management import execute_from_command_line
 
