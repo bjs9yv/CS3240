a51829229a0e4598529bb225155b30a03889e9ba
Collin Berman <collinberman1@gmail.com>
2015-11-01 22:14:51 -0500

    fix models.py

--- securecontactapp/models.py
+++ securecontactapp/models.py
@@ -1,21 +1,21 @@
 from django.db import models
 
+class User(models.Model):
+    group = models.ForeignKey('UserGroup')
+
 class UserGroup(models.Model):
     pass
 
-class User(models.Model):
-    group = models.ForeignKey(UserGroup)
-
 class Report(models.Model):
-    owner = models.ForeignKey(User)
-    folder = models.ForeignKey(Folder)
-
-class File(models.Model):
-    attached_to = models.ForeignKey(Report)
-
-class Message(models.Model):
-    sender = models.ForeignKey(User)
-    recipient = models.ForeignKey(User)
+    owner = models.ForeignKey('User')
+    folder = models.ForeignKey('Folder')
 
 class Folder(models.Model):
-    parent = models.ForeignKey(Folder)
+    parent = models.ForeignKey('Folder')
+
+class File(models.Model):
+    attached_to = models.ForeignKey('Report')
+
+class Message(models.Model):
+    sender = models.ForeignKey('User', related_name='message_sender')
+    recipient = models.ForeignKey('User', related_name='message_recipient')
