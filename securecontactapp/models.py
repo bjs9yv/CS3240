f00a66fef66608fccd7535e7f338c7e043c55e23
brian_schwartz <bjs9yv@virginia.edu>
2015-11-01 21:45:34 -0500

    added new models

--- securecontactapp/models.py
+++ securecontactapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    pass
+class UserGroup(models.Model):
+    pass
+class Report(models.Model):
+    pass
+class File(model.Model):
+    pass
+class Message(models.Model):
+    pass
+class Folder(models.Model):
+    pass
