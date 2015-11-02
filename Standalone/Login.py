8ec48538923f26a1140b0230b8bb3f669cfcd8a9
brian_schwartz <bjs9yv@virginia.edu>
2015-11-02 14:40:15 -0500

    added first-time key gen to Login script

--- Standalone/Login.py
+++ Standalone/Login.py
@@ -1,25 +1,39 @@
 __author__ = 'adminuser'
+import RSA
+import os
+import getpass
+
 
 USERNAME="bnc4vk"
 PASSWORD="password"
 
-print("Launching standalone application...\n")
+print("** Launching standalone application...\n")
 
+# lOGIN
 check = True
 while check:
-    usr = input("Enter username: ")
-    if usr == USERNAME:
+    usr = input("Enter SecureContact username: ")
+    if usr == USERNAME: # TODO: this will have to check the database online 
         check = False
         while True:
-            pswd = input("Enter password: ")
+            pswd = getpass.getpass()
             if pswd == PASSWORD:
+                print('\n')
                 break
     else:
         print("Username not recognized, please try again")
 
+# POST LOGIN PROCEDURE
+if "PrivateKey.txt" not in os.listdir(os.getcwd()): # TODO change this to search sub directory where keys are stored
+    print("** It looks like this is your first time! Generating key pair...\n")
+    RSA.gen_keys()
+    print("** Successfully created public/private key pair...\n")
+    # TODO: upload PublicKey.txt to db
+    # print("** Uploading public key to SecureContact site...\n")
+
 print("Available reports:")
 
-reports = 5
+reports = 5 # TODO: pull reports available to this username from db
 
 for i in range(5):
     print("Report [" + str(i) + "]:")
--- Standalone/RSA.py
+++ Standalone/RSA.py
@@ -1,12 +1,11 @@
 __author__ = 'adminuser'
-
-import os
 from Crypto.PublicKey import RSA
 from Crypto import Random
 
-if "PrivateKey.txt" not in os.listdir(os.getcwd()):
+def gen_keys():
     r = Random.new().read
     key = RSA.generate(4096, r)
+    # TODO: store keys in a subdirectory not current directory
     with open("PrivateKey.txt", "w") as f:
         f.write(key.exportKey(format="PEM", pkcs=8).decode())
     with open("PublicKey.txt", "w") as f:
