dd59dc8b04a9feff073805bbd981715e3bf92092
Collin Berman <collinberman1@gmail.com>
2015-11-08 13:00:33 -0500

    add templates folder to project

--- team16project/settings.py
+++ team16project/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'team16project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
