e1d084f3fd06c2f3e43397689e43095708f58e83
Matt <mhk4g@virginia.edu>
2015-11-09 17:31:54 -0500

    Standalone banner improved

--- Standalone/Login.py
+++ Standalone/Login.py
@@ -7,7 +7,29 @@ import getpass
 USERNAME="bnc4vk"
 PASSWORD="password"
 
-print("** Launching standalone application...\n")
+print("*********************************************************************************")
+print("**                                                                             **")
+print("**    .oooooo..o                                                               **")
+print("**   d8P'    `Y8                                                               **")
+print("**   Y88bo.       .ooooo.   .ooooo.  oooo  oooo  oooo d8b  .ooooo.             **")
+print("**    `'Y8888o.  d88' `88b d88' `'Y8 `888  `888  `888''8P d88' `88b            **")
+print("**        `'Y88b 888ooo888 888        888   888   888     888ooo888            **")
+print("**   oo     .d8P 888    .o 888   .o8  888   888   888     888    .o            **")
+print("**   8''88888P'  `Y8bod8P' `Y8bod8P'  `V88V'V8P' d888b    `Y8bod8P'            **")
+print("**                                                                             **")
+print("**                                                                             **")                                                               
+print("**     .oooooo.                             .                           .      **")
+print("**    d8P'  `Y8b                          .o8                         .o8      **")
+print("**   888           .ooooo.  ooo. .oo.   .o888oo  .oooo.    .ooooo.  .o888oo    **")
+print("**   888          d88' `88b `888P'Y88b    888   `P  )88b  d88' `'Y8   888      **")
+print("**   888          888   888  888   888    888    .oP'888  888         888      **")
+print("**   `88b    ooo  888   888  888   888    888 . d8(  888  888   .o8   888 .    **")
+print("**    `Y8bood8P'  `Y8bod8P' o888o o888o   '888' `Y888''8o `Y8bod8P'   '888'    **")
+print("**                                                                             **")
+print("**                                                     by Team Sixteen         **")
+print("**                                                                             **")
+print("*********************************************************************************")
+print("*********************** Launching standalone application...**********************")
 
 # lOGIN
 check = True
