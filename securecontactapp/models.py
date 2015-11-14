855e399901744a7e5a961a47e231ef9ce6b25051
Collin Berman <collinberman1@gmail.com>
2015-11-14 17:38:54 -0500

    flesh out report and file models

--- securecontactapp/models.py
+++ securecontactapp/models.py
@@ -5,14 +5,22 @@ from django.contrib.auth.models import User
 class Report(models.Model):
     owner = models.ForeignKey(User)
     folder = models.ForeignKey('Folder')
+    private = models.BooleanField()
+    text = models.TextField()
 
 # Folders to hold other Reports
 class Folder(models.Model):
     parent = models.ForeignKey('Folder')
 
+# helper function to give path to upload files to
+def report_filename(instance, filename):
+    return instance.attached_to.owner.username + '/' + filename
+
 # Uploaded files attached to Reports
 class File(models.Model):
     attached_to = models.ForeignKey('Report')
+    encrypted = models.BooleanField()
+    file = models.FileField(upload_to=report_filename)
 
 # Message sent from one user to another
 class Message(models.Model):
