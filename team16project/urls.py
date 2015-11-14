28e2fba265e65255d675a94681ab295a22e9748c
brian_schwartz <bjs9yv@virginia.edu>
2015-11-14 15:53:06 -0500

    making userbase links active, adding reports page

--- team16project/urls.py
+++ team16project/urls.py
@@ -9,7 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^$', 'securecontactapp.views.home', name='home'),
-    url(r'^reports$', 'securecontactapp.views.reports', name='reports'),
+    url(r'^reports/$', 'securecontactapp.views.reports', name='reports'),
     url(r'^register/$', 'securecontactapp.views.registration', name='register'),
     url(r'^check_login/$', 'securecontactapp.views.check_login', name='check_login'),
 )
